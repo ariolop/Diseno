d075c36ece0c69e6692b947c593fe157535d7f23
Alejandro Ríos López <99210080+ariolop@users.noreply.github.com>
2024-10-24 22:04:54 +0200

    Delete Ejercicios02-14 directory

--- Ejercicios02-14/src/js/index.js
+++
@@ -1,6 +0,0 @@
-document.getElementById("botonOscuro").addEventListener(('click'), () => {
-    document.documentElement.classList.toggle("dark");
-    document.getElementById("iconoOscuro").classList.toggle("bi-moon");
-    document.getElementById("iconoOscuro").classList.toggle("bi-sun");
-});
-

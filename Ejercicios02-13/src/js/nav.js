edc2edfb514b1849d49c08fa4fc0f7a42676dfcf
Alejandro Ríos López <99210080+ariolop@users.noreply.github.com>
2024-10-24 22:03:06 +0200

    Delete Ejercicios02-13 directory

--- Ejercicios02-13/src/js/nav.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-document.getElementById("menuBurguer").addEventListener( ("click"), () => {
-    console.log("click");
-    document.getElementById("menu").classList.toggle("hidden");
-    // document.getElementById("nav").classList.toggle(`h-[6vh]`);
-});
